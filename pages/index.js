419e91e65a9bd6f7e6ed66dfe0c723df49045d30
Abraham Bueno <abueno@localhost.localdomain>
2018-10-28 23:34:22 -0500

    Setup for _document.js for styling, meta tags, etc. Also added Header component, and Index.js with SEO meta tags.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-foo(
-  reallyLongArg(),
-  reallyReallyLongArg(),
-  omgSoManyParameters(),
-  IShouldRefactorThis(),
-  isThereSeriouslyAnotherOne(),
+// import React from 'react';
+import Head from 'next/head';
+import Header from '../components/Header';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index Page</title>
+      <meta name="description" content="This is the description of the Index Page" />
+    </Head>
+    <Header />
+    <p>Content of the Index Page</p>
+  </div>
 );
+
+export default Index;
