582ff53d28fc8d562b63a99616cb28c5bef68fbc
Abraham Bueno <abueno@localhost.localdomain>
2018-10-30 17:22:17 -0500

    Express server now working.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
-// import React from 'react';
+import PropTypes from 'prop-types';
+
 import Head from 'next/head';
-import Button from '@material-ui/core/Button';
+
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
-      <title>Index Page</title>
-      <meta name="description" content="This is the description of the Index Page" />
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
     </Head>
-    <p>Content of the Index Page</p>
-    <Button variant="contained">MUI button</Button>
+    <p>Content on Index page</p>
+    <p>
+Email:
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);
