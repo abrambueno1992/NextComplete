1fefbe3ba9c2528ab86df788a07fa4f7e9f22c0c
Abraham Bueno <abueno@localhost.localdomain>
2018-11-09 17:56:32 -0600

    Added the methods to the Book and Chapter models for handling github requests.

--- server/models/Chapter.js
+++ server/models/Chapter.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-import Book from './Book';
+import generateSlug from '../utils/slugify';
+import Book from './Book'
 
 const { Schema } = mongoose;
 
@@ -67,6 +68,72 @@ class ChapterClass {
     chapterObj.book = book;
 
     return chapterObj;
+  }
+  static async syncContent({ book, data }) {
+    const {
+      title,
+      excerpt = '',
+      isFree = false,
+      seoTitle = '',
+      seoDescription = '',
+    } = data.attributes;
+  
+    const { body, path } = data;
+  
+    const chapter = await this.findOne({
+      bookId: book.id,
+      githubFilePath: path,
+    });
+  
+    let order;
+  
+    if (path === 'introduction.md') {
+      order = 1;
+    } else {
+      order = parseInt(path.match(/[0-9]+/), 10) + 1;
+    }
+  
+    if (!chapter) {
+      const slug = await generateSlug(this, title, { bookId: book._id });
+  
+      return this.create({
+        bookId: book._id,
+        githubFilePath: path,
+        title,
+        slug,
+        isFree,
+        content,
+        htmlContent,
+        sections,
+        excerpt,
+        htmlExcerpt,
+        order,
+        seoTitle,
+        seoDescription,
+        createdAt: new Date(),
+      });
+    }
+  
+    const modifier = {
+      content,
+      htmlContent,
+      sections,
+      excerpt,
+      htmlExcerpt
+      isFree,
+      order,
+      seoTitle,
+      seoDescription,
+    };
+  
+    if (title !== chapter.title) {
+      modifier.title = title;
+      modifier.slug = await generateSlug(this, title, {
+        bookId: chapter.bookId,
+      });
+    }
+  
+    return this.updateOne({ _id: chapter._id }, { $set: modifier });
   }
 }
 
